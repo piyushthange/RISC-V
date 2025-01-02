759b494235a90aceca0967145ca9862f5a0b78ea
Piyush Thange <pthange19@gmail.com>
2025-01-02 22:38:58 +0530

    QEMU run script for the RISC-V
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ run_script.sh
@@ -0,0 +1,12 @@
+qemu-system-riscv64 -machine	 virt \
+		    -cpu 	 rv64 \
+		    -m		 8G   \
+		    -smp	 8    \
+		    -device	 virtio-blk-device,drive=hd -drive file=ubunturv.img,if=none,id=hd \
+		    -device	 virtio-net-device,netdev=net -netdev user,id=net,hostfwd=tcp::2222-:22 \
+		    -bios	/usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.elf  \
+		    -kernel	/usr/lib/u-boot/qemu-riscv64_smode/uboot.elf  \
+		    -object	rng-random,filename=/dev/urandom,id=rng  \
+		    -device	virtio-rng-device,rng=rng   \
+		    -append	"root=LABEL=rootfs console=ttyS0" \
+		    -nographic
